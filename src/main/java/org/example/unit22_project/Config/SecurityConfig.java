931f581b7080dd4c3241541ffcf613c1c5db19f4
Trytofindme2 <doed2792@gmail.com>
2025-01-06 21:01:41 +0630

    Spring Security

---
+++ src/main/java/org/example/unit22_project/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.unit22_project.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers("/index/admin/**").authenticated()
+                        .requestMatchers("/index/doctor/**").authenticated()
+                        .requestMatchers("/index/user/**").authenticated()
+                        .anyRequest().permitAll());
+
+        http.formLogin(customizer ->
+                customizer.disable());
+        http.logout(customizer ->
+                customizer.logoutUrl("/logout").permitAll());
+        return http.build();
+    }
+}
